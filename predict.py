f267b15285633383b04a2efe72a7e3bbd81690fa
fdash <sfukuda.fdash@gmail.com>
2016-02-15 07:41:08 +0900

    Rename variables.

--- predict.py
+++ predict.py
@@ -23,18 +23,18 @@ def pp(obj):
   str = pp.pformat(obj)
   return re.sub(r"\\u([0-9a-f]{4})", lambda x: unichr(int("0x"+x.group(1), 16)), str)
 
-def fit_and_predict(estimator, data_train, label_train, data_train2, label_train2, line_number, dictionary):
+def fit_and_predict(estimator, data_train, label_train, data_test, label_test, line_number, dictionary):
   # fitting
   estimator.fit(data_train, label_train)
 
   # prediction
-  label_predict = estimator.predict(data_train2)
+  label_predict = estimator.predict(data_test)
 
   ary = [[0 for i in range(2)] for j in range(2)]
   correct = 0
-  for i in range(0,len(data_train2)):
-    ary[int(label_train2[i])-1][int(label_predict[i])-1] += 1
-    diff = int(label_train2[i])  - int(label_predict[i])
+  for i in range(0,len(data_test)):
+    ary[int(label_test[i])-1][int(label_predict[i])-1] += 1
+    diff = int(label_test[i])  - int(label_predict[i])
     if diff == 0:
       correct += 1
 
@@ -49,9 +49,8 @@ def fit_and_predict(estimator, data_train, label_train, data_train2, label_train
     print "{0:03d}".format(ary[1][prediction]),
   print ""
 
-  print >> sys.stderr, "accuracy: " + str(correct) + " / " + str(len(data_train2))
+  print >> sys.stderr, "accuracy: " + str(correct) + " / " + str(len(data_test))
 
-  #coef = estimator.get_coef()
   coef = estimator.coef_
   items = sorted(dictionary.items())
   term_coef = {}
@@ -63,14 +62,14 @@ def fit_and_predict(estimator, data_train, label_train, data_train2, label_train
   factors = []
   for i in range(NUM_FACTORS):
     factors.append((str(term_coef[i][0][0]) + " : " + pp(term_coef[i][0][1]) + " : " + str(term_coef[i][1]) + "\n").encode('utf-8'))
-  return [(float(correct) / len(data_train2)), factors]
+  return [(float(correct) / len(data_test)), factors]
 
 
 def make_data(dictionary, tfidf_corpus, labels, line_ids, offset=0):
-  data_train   = []
-  data_train2  = []
-  label_train  = []
-  label_train2 = []
+  data_train  = []
+  data_test   = []
+  label_train = []
+  label_test  = []
   line_number = []
   i = 0
 
@@ -82,8 +81,8 @@ def make_data(dictionary, tfidf_corpus, labels, line_ids, offset=0):
     for doc in tfidf_corpus:
       dense = list(gensim.matutils.corpus2dense([doc], num_terms=len(dictionary)).T[0])
       if ((i + offset) % (num_document / TEST )) == 0 and num_test < TEST and stars[int(labels[i])] < (TEST / NUM_STAR):
-        data_train2.append(dense)
-        label_train2.append(labels[i])
+        data_test.append(dense)
+        label_test.append(labels[i])
         stars[int(labels[i])] += 1
         line_number.append(line_ids[i])
         num_test += 1
@@ -97,7 +96,7 @@ def make_data(dictionary, tfidf_corpus, labels, line_ids, offset=0):
       offset = (offset + 1) % TEST_COUNT
       i = 0
       is_filling = True
-  return [data_train, label_train, data_train2, label_train2, line_number]
+  return [data_train, label_train, data_test, label_test, line_number]
 
 def main():
   documents = []
@@ -135,11 +134,11 @@ def main():
   max_factors = []
 ## MAIN LOOP
   for i in range(0, TEST_COUNT):
-    [data_train, label_train, data_train2, label_train2, line_target] = make_data(dictionary, tfidf_corpus, labels, line_ids, offset = i)
+    [data_train, label_train, data_test, label_test, line_target] = make_data(dictionary, tfidf_corpus, labels, line_ids, offset = i)
 
     estimator = LinearSVC(C=0.25);
 
-    accuracy, factors = fit_and_predict(estimator, data_train, label_train, data_train2, label_train2, line_target, dictionary)
+    accuracy, factors = fit_and_predict(estimator, data_train, label_train, data_test, label_test, line_target, dictionary)
     if accuracy > max_accuracy:
       max_factors = factors
     result.append(accuracy)
@@ -155,5 +154,5 @@ def main():
   f.close()
 
 if __name__ == '__main__':
-  main();
+  main()
 
